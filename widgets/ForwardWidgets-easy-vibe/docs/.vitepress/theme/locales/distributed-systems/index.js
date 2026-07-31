b5b9fbd0f8c83f911cca121f3f616b24bbf69970
Amuro Ray <88413337+FreeWillDeveloper@users.noreply.github.com>
2026-08-01 00:38:02 +0800

    Delete widgets/ForwardWidgets-easy-vibe directory

--- widgets/ForwardWidgets-easy-vibe/docs/.vitepress/theme/locales/distributed-systems/index.js
+++
@@ -1,8 +0,0 @@
-import zhCn from './zh-cn.js'
-import en from './en.js'
-
-export const distributedSystemsLocale = {
-  'zh-cn': zhCn,
-  en
-}
-
--- widgets/ForwardWidgets-easy-vibe/docs/.vitepress/theme/locales/system-design-methodology/index.js
+++
@@ -1,8 +0,0 @@
-import zhCn from './zh-cn.js'
-import en from './en.js'
-
-export const systemDesignMethodologyLocale = {
-  'zh-cn': zhCn,
-  en
-}
-
