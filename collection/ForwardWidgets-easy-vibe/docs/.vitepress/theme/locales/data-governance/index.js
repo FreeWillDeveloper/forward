1c2db24b46857f988cac679b6625749d098d56b3
Amuro Ray <88413337+FreeWillDeveloper@users.noreply.github.com>
2026-08-01 17:03:24 +0800

    Delete collection/ForwardWidgets-easy-vibe directory

--- collection/ForwardWidgets-easy-vibe/docs/.vitepress/theme/locales/data-governance/index.js
+++
@@ -1,8 +0,0 @@
-import zhCn from './zh-cn.js'
-import en from './en.js'
-
-export const dataGovernanceLocale = {
-  'zh-cn': zhCn,
-  en
-}
-
--- collection/ForwardWidgets-easy-vibe/docs/.vitepress/theme/locales/server-backend/index.js
+++
@@ -1,8 +0,0 @@
-import zhCn from './zh-cn.js'
-import en from './en.js'
-
-export const serverBackendLocale = {
-  'zh-cn': zhCn,
-  en
-}
-
